144725afdf691028f2cdd5385de878d3507ea567
jastring <tripp.stringfield@gmail.com>
2019-10-30 15:20:06 -0400

    added route for searching historical data between specific dates

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,25 +1,26 @@
 var db = require("../models");
+var Sequelize = require("sequelize");
+var Op = Sequelize.Op;
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.render("index", {
         msg: "Welcome!",
         examples: dbExamples
       });
     });
   });
-
-  // Load example page and pass in an example by id
-  app.get("/:symbol/:date", function(req, res) {
+  // Renders example.handlebars with an object containing stock data for a specific day
+  app.get("/historical/:symbol/:date", function (req, res) {
     db.stockEntries
       .findAll({
         where: { symbol: req.params.symbol, specificDate: req.params.date }
       })
-      .then(function(dbExample) {
-        var singleDay = dbExample[0].dataValues;
-        var returnObj = {
+      .then(function (data) {
+        var singleDay = data[0].dataValues;
+        var stockObj = {
           symbol: singleDay.symbol,
           date: singleDay.specificDate,
           open: singleDay.openVal,
@@ -33,13 +34,47 @@ module.exports = function(app) {
           volume: singleDay.volume
         };
         res.render("example", {
+          example: stockObj
+        });
+      });
+  });
+  //This returns an Array of objects that contain stock information between the two given dates passed in the url
+  app.get("/historical/:symbol/:start/:end", function (req, res) {
+    db.stockEntries
+      .findAll({
+        where: { symbol: req.params.symbol, specificDate: { [Op.between]: [req.params.start, req.params.end] } }
+      })
+      .then(function (data) {
+        var stockArray = [];
+        for (var i = 0; i < data.length; i++) {
+          var singleDay = data[i].dataValues;
+          var stockObj = {
+            date: singleDay.specificDate,
+            high: singleDay.highVal,
+            low: singleDay.lowVal,
+            open: singleDay.openVal,
+            close: singleDay.closeVal,
+            volume: singleDay.volume
+          }
+          stockArray.push(stockObj);
+        }
+        var returnObj = {
+          symbol: data[0].dataValues.symbol,
+          startDate: stockArray[0].date,
+          endDate: stockArray[stockArray.length - 1].date,
+          open: stockArray[0].open,
+          close: stockArray[stockArray.length - 1].close,
+          change: ((((stockArray[stockArray.length - 1].close) - stockArray[0].open) / stockArray[0].open) * 100).toPrecision(4)
+        }
+        console.log(returnObj);
+        res.render("example2", {
           example: returnObj
         });
       });
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     res.render("404");
   });
-};
+}
